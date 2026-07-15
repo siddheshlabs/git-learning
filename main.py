848669f7dd48081f24ee40e8e877e4e5ef1d6a2a
siddheshlabs <siddheshvetale@gmail.com>
2026-07-15 15:57:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-# Import the add() function from utils.py
+# Import helper function
 from utils import add
 
+# Import calculator function
+from calculator import multiply
+
+# Existing functionality
 print("Hello Git")
 print(add(10, 20))
+
+# New functionality
+print("Multiplication:", multiply(5, 6))
